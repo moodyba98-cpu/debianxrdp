64a2c39e3d2abd69b774c138b9940725e7c8ed73
moodyba98-cpu <moodyba98@gmail.com>
2026-09-24 09:44:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,20 @@
+
 #!/bin/bash
 
-service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
-service xrdp start
+set -e
 
+# Start DBus
+service dbus start
+
+# Start PulseAudio
+pulseaudio --start --system --disallow-exit --disable-shm || true
+
+# Prepare X11 socket
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-tail -f /var/log/xrdp-sesman.log
+# Start XRDP
+service xrdp start
+
+# Keep container alive and show XRDP logs
+tail -F /var/log/xrdp/xrdp.log /var/log/xrdp/xrdp-sesman.log
